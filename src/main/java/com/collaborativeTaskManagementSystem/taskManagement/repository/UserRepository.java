d6e59fc74520ba611f090a2105f1ca377216bb57
ArseneDellamis <dellamis11@gmail.com>
2025-05-28 17:23:36 +0200

    removed all methods in project service and controller

--- src/main/java/com/collaborativeTaskManagementSystem/taskManagement/repository/UserRepository.java
+++ src/main/java/com/collaborativeTaskManagementSystem/taskManagement/repository/UserRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmailIgnoreCaseOrUsernameIgnoreCase(String email, String username);
+    Optional<User> findByEmailIgnoreCase(String email);
 }
